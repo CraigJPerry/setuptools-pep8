a385fc8d6c67500a7d9d8987b4a109c0c1f3cab2
Craig J Perry <craigp84@gmail.com>
2013-10-03 21:38:42 +0100

    Updated setup.py to reflect change pylint -> pep8.

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""pep8 command for setuptools"""
+
+
 from setuptools import setup, find_packages
 import sys, os
 
+
+VERSION = '0.1'
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.1'
-
-install_requires = [
-    'pylint'
-]
-
-
-setup(name='setuptools-lint',
-    version=version,
-    description="Setuptools command for pylint",
+setup(name='setuptools-pep8',
+    version=VERSION,
+    description=__doc__,
     long_description=README + '\n\n' + NEWS,
     classifiers=[
         "Topic :: Documentation",
@@ -28,19 +30,20 @@ setup(name='setuptools-lint',
         "Topic :: Software Development :: Documentation",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    keywords='pylint setuptools command',
-    author='Xavier Barbosa',
-    author_email='',
-    url='https://github.com/johnnoone/setuptools-pylint',
+    keywords='pep8 setuptools command',
+    author='Craig J Perry',
+    author_email='craigp84@gmail.com',
+    url='https://github.com/CraigJPerry/setuptools-pep8',
     license='BSD',
     packages=find_packages('src'),
     package_dir = {'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
+    install_requires=['pep8'],
     entry_points={
         "distutils.commands": [
-            "lint = setuptools_lint.setuptools_command:PylintCommand",
+            "pep8 = setuptools_pep8.setuptools_command:Pep8Command",
         ]
     }
 )
+

c385308b29538cb0db22856317aa1a58e8cf4f79
Craig J Perry <craigp84@gmail.com>
2013-12-03 00:24:07 +0000

    Added test case to replicate [issue 3](https://github.com/CraigJPerry/setuptools-pep8/issues/3)
    Currently failing.

--- src/tests/test_pep8_command.py
+++ src/tests/test_pep8_command.py
@@ -4,13 +4,23 @@
 
 import unittest
 import setuptools
+from os.path import abspath, join, dirname
 from setuptools_pep8.setuptools_command import Pep8Command
+import subprocess
+
+
+FIXTURES_DIR = abspath(join(dirname(__file__), "fixtures"))
 
 
 class SetupCfgDirectives(unittest.TestCase):
 
     def test_case_is_setup_correctly(self):
         self.assertTrue(issubclass(Pep8Command, setuptools.Command))
+
+    def test_can_use_excludes_parameter_in_setup_cfg(self):
+        cmdline = ['python', 'setup.py', 'pep8']
+        output = subprocess.check_output(cmdline, stderr=subprocess.STDOUT, cwd=FIXTURES_DIR)
+        self.assertNotIn("excdir", output)
 
 
 if __name__ == '__main__':

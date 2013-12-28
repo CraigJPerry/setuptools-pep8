7f9ab14974e90d98c1155d598d005cd49ec56b56
Craig J Perry <craigp84@gmail.com>
2013-12-28 23:49:33 +0000

    Minor reformatting

--- setup.py
+++ setup.py
@@ -17,6 +17,23 @@ setup(
     version=__version__,
     description=__doc__,
     long_description=README + '\n\n' + NEWS,
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['pep8'],
+    entry_points={
+        "distutils.commands": [
+            "pep8 = setuptools_pep8.setuptools_command:Pep8Command",
+        ]
+    },
+
+    author='Craig J Perry',
+    author_email='craigp84@gmail.com',
+    url='https://github.com/CraigJPerry/setuptools-pep8',
+    license='BSD',
+    keywords='pep8 setuptools command',
     classifiers=[
         "Topic :: Documentation",
         "Framework :: Setuptools Plugin",
@@ -27,21 +44,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         "Topic :: Software Development :: Documentation",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords='pep8 setuptools command',
-    author='Craig J Perry',
-    author_email='craigp84@gmail.com',
-    url='https://github.com/CraigJPerry/setuptools-pep8',
-    license='BSD',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=['pep8'],
-    entry_points={
-        "distutils.commands": [
-            "pep8 = setuptools_pep8.setuptools_command:Pep8Command",
-        ]
-    }
+    ]
 )
 
